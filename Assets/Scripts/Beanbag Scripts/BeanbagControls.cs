259de5cb5f45d3d21083f9d7e9ca7225c2a53828
Dominic Bellisario (RIT Student) <dlb5102@rit.edu>
2025-01-30 10:45:02 -0500

    fixed lots of physics bugs and added random rotation to beanbag
    disabling the rb of beanbag before it launches stops it brom building up speed

--- Assets/Scripts/Beanbag Scripts/BeanbagControls.cs
+++ Assets/Scripts/Beanbag Scripts/BeanbagControls.cs
@@ -7,6 +7,9 @@ public class BeanbagControls : ObjectControls
     [SerializeField]
     int heightMultipliter;
 
+    [SerializeField]
+    float rotRange;
+
     public Vector3 PredictVelocity(float _angle)
     {
         return new Vector3(-powerPercent * Mathf.Sin(_angle) * forceMultiplyer, powerPercent * heightMultipliter, -powerPercent * Mathf.Cos(_angle) * forceMultiplyer);
@@ -18,34 +21,30 @@ public class BeanbagControls : ObjectControls
     protected override void LaunchObject()
     {
         rb.AddForce(-powerPercent * Mathf.Sin(angle) * forceMultiplyer, powerPercent * heightMultipliter, -powerPercent * Mathf.Cos(angle) * forceMultiplyer);
+        rb.AddTorque(new Vector3(Random.Range(-rotRange, rotRange), Random.Range(-rotRange, rotRange), Random.Range(-rotRange, rotRange)));
     }
 
     /// <summary>
-    /// spawn new beanbags and give them this beanbags force at a slight angle
+    /// spawn new beanbags, give them this beanbags force at a slight angle, give them the powerup states of this bean bag, and give them a random rotation
     /// </summary>
     /// <param name="effects"></param>
     protected override void SpawnNewTriObjects(ObjectEffects effects)
     {
-        GameObject leftObject = LevelManager.Instance.SpawnNewObject(
+        SpawnTriBeanbag(effects, -.5f, -triBallAngleRads);
+        SpawnTriBeanbag(effects, .5f, triBallAngleRads);
+    }
+
+    void SpawnTriBeanbag(ObjectEffects effects, float triPos, float triBallAngle)
+    {
+        GameObject beanbag = LevelManager.Instance.SpawnNewObject(
             gameObject,
-            new Vector3(gameObject.transform.position.x - .5f, gameObject.transform.position.y, gameObject.transform.position.z),
+            new Vector3(gameObject.transform.position.x + triPos, gameObject.transform.position.y, gameObject.transform.position.z),
             new Vector3(
-                -powerPercent * Mathf.Sin(angle - triBallAngleRads) * forceMultiplyer,
+                -powerPercent * Mathf.Sin(angle + triBallAngle) * forceMultiplyer,
                 powerPercent * heightMultipliter,
-                -powerPercent * Mathf.Cos(angle - triBallAngleRads) * forceMultiplyer),
+                -powerPercent * Mathf.Cos(angle + triBallAngle) * forceMultiplyer),
             effects.GoldBallEnabled, effects.MarkedBallEnabled, false);
-        leftObject.GetComponent<ObjectLevelInteractions>().IsLaunched = true;
-
-        GameObject rightObject = LevelManager.Instance.SpawnNewObject(
-            gameObject,
-            new Vector3(gameObject.transform.position.x + .5f, gameObject.transform.position.y, gameObject.transform.position.z),
-            new Vector3(
-                -powerPercent * Mathf.Sin(angle + triBallAngleRads) * forceMultiplyer,
-                powerPercent * heightMultipliter,
-                -powerPercent * Mathf.Cos(angle + triBallAngleRads) * forceMultiplyer),
-            effects.GoldBallEnabled, effects.MarkedBallEnabled, false);
-        rightObject.GetComponent<ObjectLevelInteractions>().IsLaunched = true;
-
-
+        beanbag.GetComponent<ObjectLevelInteractions>().IsLaunched = true;
+        beanbag.GetComponent<Rigidbody>().AddTorque(new Vector3(Random.Range(-rotRange, rotRange), Random.Range(-rotRange, rotRange), Random.Range(-rotRange, rotRange)));
     }
 }
