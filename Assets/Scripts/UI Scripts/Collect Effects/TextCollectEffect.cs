07931ff165549ae52fdf4cb1f4fc095037558545
DominicBellisario <dlb5102@g.rit.edu>
2025-05-27 19:37:24 -0400

    Edited Powerup Activate sound and added score update sound

--- Assets/Scripts/UI Scripts/Collect Effects/TextCollectEffect.cs
+++ Assets/Scripts/UI Scripts/Collect Effects/TextCollectEffect.cs
@@ -78,8 +78,11 @@ public class TextCollectEffect : MonoBehaviour
             yield return new WaitForEndOfFrame();
         }
 
-        //once it reaches, update the function needed
+        //once it reaches:
+        //update the function needed
         LevelUILogic.Instance.Invoke(functionToCall, 0);
+        //play score update sound with a random pitch
+        SoundManager.Instance.PlaySound(15, 0.75f, 0.75f + Helper.Instance.RandomInt(1, 50) * 0.01f);
 
         //then, destroy it
         Destroy(gameObject);
